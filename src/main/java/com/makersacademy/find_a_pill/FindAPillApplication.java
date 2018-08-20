5d6e03dc0da7c137cb3e755f0ed81e26ec94a718
George Sykes <georgesykes86@gmail.com>
2018-08-20 12:24:50 +0100

    Add basic project SetUp

---
+++ src/main/java/com/makersacademy/find_a_pill/FindAPillApplication.java
@@ -0,0 +1,12 @@
+package com.makersacademy.find_a_pill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAPillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAPillApplication.class, args);
+	}
+}
