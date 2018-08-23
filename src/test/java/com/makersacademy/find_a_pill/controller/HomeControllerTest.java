71034d2d6d654f89eb469773c32675e3871c914f
Asia Antczak <aasiaantczak@gmail.com>
2018-08-23 11:04:34 +0100

    Add root

---
+++ src/test/java/com/makersacademy/find_a_pill/controller/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package com.makersacademy.find_a_pill.controller;
+
+import com.makersacademy.find_a_pill.service.PillService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @MockBean
+    private HomeController controller;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void indexRootReturnsGreeting() {
+        when(controller.index()).thenCallRealMethod();
+        assertEquals("Hello World", controller.index());
+    }
+
+}
